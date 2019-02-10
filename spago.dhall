e1638768f2c758741db42fdd5f7b209de707e899
bouzuya <m@bouzuya.net>
2019-02-10 13:36:47 +0900

    add initial implement

--- spago.dhall
+++ spago.dhall
@@ -5,7 +5,7 @@ You can edit this file as you like.
 { name =
     "my-project"
 , dependencies =
-    [ "effect", "console" ]
+    [ "console", "effect", "node-fs-aff", "node-process" ]
 , packages =
     ./packages.dhall
 }
--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,24 @@ module Main where
 
 import Prelude
 
+import Data.Array as Array
+import Data.Maybe as Maybe
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Aff as Aff
+import Effect.Class (liftEffect)
+import Effect.Class.Console as Console
+import Node.Encoding as Encoding
+import Node.FS.Aff as Fs
+import Node.Process as Process
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main = Aff.launchAff_ do
+  argv <- liftEffect (Process.argv)
+  fileMaybe <- pure (Array.index argv 2)
+  file <-
+    Maybe.maybe
+      (Aff.throwError (Aff.error "No file"))
+      pure
+      fileMaybe
+  text <- Fs.readTextFile Encoding.UTF8 file
+  Console.log text
