5c424f302e459bf8e7b28c941665940f1644c588
bouzuya <m@bouzuya.net>
2019-02-10 14:00:48 +0900

    add readFromStream

--- src/Main.purs
+++ src/Main.purs
@@ -8,18 +8,32 @@ import Effect (Effect)
 import Effect.Aff as Aff
 import Effect.Class (liftEffect)
 import Effect.Class.Console as Console
+import Effect.Ref as Ref
 import Node.Encoding as Encoding
 import Node.FS.Aff as Fs
 import Node.Process as Process
+import Node.Stream as Stream
+
+readFromStream :: Stream.Readable () -> Aff.Aff String
+readFromStream r =
+  Aff.makeAff
+    (\callback -> do
+      ref <- Ref.new ""
+      Stream.onDataString r Encoding.UTF8 \s -> do
+        buffer <- Ref.read ref
+        Ref.write (buffer <> s) ref
+      Stream.onEnd r do
+        buffer <- Ref.read ref
+        callback (pure buffer)
+      pure mempty)
 
 main :: Effect Unit
 main = Aff.launchAff_ do
   argv <- liftEffect (Process.argv)
   fileMaybe <- pure (Array.index argv 2)
-  file <-
+  text <-
     Maybe.maybe
-      (Aff.throwError (Aff.error "No file"))
-      pure
+      (readFromStream Process.stdin)
+      (\file -> Fs.readTextFile Encoding.UTF8 file)
       fileMaybe
-  text <- Fs.readTextFile Encoding.UTF8 file
   Console.log text
